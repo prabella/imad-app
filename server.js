a423f9ebc43cee6194f10d71f067a11dc0c0b84d
prabella <30681947+prabella@users.noreply.github.com>
2017-08-22 12:42:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
-app.get('/ui/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
