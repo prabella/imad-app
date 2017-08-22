3f72111d0bd299cc31fd1229ace2321ba81ba1e6
prabella <30681947+prabella@users.noreply.github.com>
2017-08-22 12:25:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,43 +1,28 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
-});
-
-app.get('/article-one',function(req,res){
-    res.send("Article one requested and will be served here") 
-});
-
-app.get('/article-two',function(req,res){
-    res.send("Article two requested and will be served here") 
-});
-
-app.get('/artcile-three',function(req,res){
-   res.send("Article three requested is served here") 
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+<html>
+    <head>
+        <title>
+            Praveen Bellary's Page
+        </title>
+    </head>
+    <body>
+        <div>
+            <a href='/'>Home</a>
+        </div>    
+            <h3>
+                Article One
+            </h3>
+            <div>
+                Aug 22,2017
+            </div>
+        <p>
+            This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.
+        </p>   
+         <p>
+            This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.
+        </p>   
+         <p>
+            This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.This is my first Article.
+        </p>   
+        
+    </body>
+</html>
