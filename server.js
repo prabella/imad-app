3ef39f612a35fa5b76a318257ec746de664ae4dd
prabella <30681947+prabella@users.noreply.github.com>
2017-08-22 14:52:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/article-one.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
