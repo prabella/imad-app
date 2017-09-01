879228cb0468c317db3c597ac3bd4a97f356eb32
prabella <30681947+prabella@users.noreply.github.com>
2017-09-01 14:47:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','artcile-two.html'));
+    res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 
-app.get('article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 
