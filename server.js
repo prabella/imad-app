262e4c2e28d5096d10df9473f57b1d7fc977ba07
prabella <30681947+prabella@users.noreply.github.com>
2017-09-04 15:18:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,11 +65,17 @@ var htmlTemplate=`<html>
 </html>
 `;
 return htmlTemplate;
-};
+}
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','index.html'));
+});
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+    res.send(counter.toString());
 });
 
 app.get('/:articleName', function (req, res) {
