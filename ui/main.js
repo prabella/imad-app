35d596c67255b7b128545c64bd2c59a01774ae5c
prabella <30681947+prabella@users.noreply.github.com>
2017-09-04 18:16:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,8 @@ submit.onclick = function(){
          } 
          
      };
-  
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://praveenbellaryp.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
