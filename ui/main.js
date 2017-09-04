f534040b913116366f6389dc4a674286fe55acd0
prabella <30681947+prabella@users.noreply.github.com>
2017-09-04 18:14:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,13 +22,27 @@ button.onclick = function(){
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-  
-  var names = ['name1','name2','name3','name4'];
+   //create a request
+    var request = new XMLHttpRequest();
+     request.onreadystatechange = function(){
+         if(request.readyState === XMLHttpRequest.DONE){
+             if(request.status === 200){
+                  
+              var names = request.responseText;
+              names = JSON.parse(names);
   var list = '';
   for(i=0;i<names.length;i++){
       
       list += '<li>'+names[i]+'</li>';
   }
   var ul = document.getElementById('namelist');
-  ul.innerHTML= list;
+      ul.innerHTML= list;         
+                 
+             }
+         } 
+         
+     };
+  
+    request.open('GET','http://praveenbellaryp.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
