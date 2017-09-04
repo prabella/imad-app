4ecc37496554fba2e830c4385f3c01bff2ad84f3
prabella <30681947+prabella@users.noreply.github.com>
2017-09-04 14:43:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var element = document.getElementById('main-text');
 element.innerHTML= 'New Value';
 
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
    marginLeft=marginLeft+10;
    img.style.marginLeft=marginLeft+'px';
