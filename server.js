bee996ebd9ce4082fb1e335399b84c4f4d85d8a5
prabella <30681947+prabella@users.noreply.github.com>
2017-09-05 10:37:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','index.html'));
 });
 
-var pool = new Pool();
+var pool = new Pool(config);
 app.get('/test_db',function(req,res){
     
     pool.query('SELECT * FROM test',function(err,result){
